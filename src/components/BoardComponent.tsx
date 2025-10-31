14b9e347131dd6ffd0f3deecc3134138913b1f0b
ANABIXS <vladkolumbus@gmail.com>
2025-10-31 10:35:35 +0100

    first commit

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Board } from "../modules/Board";
+import CellComponent from "./CellComponent";
+import { FC } from "react";
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void;
+}
+
+const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  return (
+    <div className="board">
+      {board.cells.map((row, index) => (
+        <React.Fragment key={index}>
+          {row.map((cell) => (
+            <CellComponent cell={cell} key={cell.id} />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default BoardComponent;
